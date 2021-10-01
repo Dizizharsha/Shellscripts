9d99e132b9464fe06cc98cade83b016780f41a5e
Dizizharsha <33566118+Dizizharsha@users.noreply.github.com>
2021-10-01 22:47:19 +0530

    Update sumscript.sh

--- sumscript.sh
+++ sumscript.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # Calculate the sum via command-line arguments
-# $1 and $2 refers to the first and second argument passed as command-line arguments
-
-parameter_one=${1}
-parameter_two=${2}
-
-echo "Parameter 1 = ${parameter_one}"
-echo "Parameter 2 = ${parameter_two}"
 
 sum=$(( ${parameter_one} + ${parameter_two} ))
 
